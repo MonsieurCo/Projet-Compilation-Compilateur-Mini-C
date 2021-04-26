c4b79c151512c268c343ee79a67d4f66c2748cdd
Margaux Schmied <margaux.schmied@icloud.com>
2021-04-26 09:03:39 +0200

    modif

--- table.h
+++ table.h
@@ -1,10 +1,20 @@
 #define TAILLE 103 /* nombre premier de préférence */
 
+typedef enum {INT, VOID} type_t;
+
 typedef struct _symbole { 
     char *nom;
-    double valeur;
+    //double valeur;
+    //type_t type;
     struct _symbole *suivant;
 } symbole;
+
+typedef struct _tree {
+    char *nom; 
+    struct _tree *suivants;
+} tree;
+
+
 
 symbole * inserer( char *nom );
 
