a7dd8d699e0db3dc089c9298f97a8d2a93877b88
MonsieurCo <monsieur.cousson@gmail.com>
2021-04-27 23:41:28 +0200

    fin de journée

---
+++ Tests/test.c
@@ -0,0 +1,13 @@
+int main() {
+    int x, y, z, t, toto, a;
+    x = -900;
+    if (x < 10) {
+        if (z > 10) {
+            y = 500;
+        }
+        a = 63;
+        return a;
+    } else {
+        return -1;
+    }
+}
